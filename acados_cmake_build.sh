dc68ca7cd4885fd4667aa0e929d515124ef55c15
yuzhangbit <yu.zhang.bit@gmail.com>
2021-08-21 14:49:09 +0800

    add osqp patch and install script

---
+++ acados_cmake_build.sh
@@ -0,0 +1,24 @@
+set -e
+
+# submodule clone
+mkdir -p ${HOME}/acados_osqp6
+
+cd /tmp
+rm -rf acados && git clone https://github.com/yuzhangbit/acados.git && 
+cd /tmp/acados && git checkout tag_osqp_0.6 && git submodule update --recursive --init
+
+# apply patches
+cd /tmp/acados && patch external/osqp/src/util.c -i 0001-fix-profiling-bug.patch
+# compiling
+cd /tmp/acados && mkdir -p build && cd /tmp/acados/build
+cmake ..  -DACADOS_UNIT_TESTS=TRUE   \
+          -DACADOS_WITH_QPOASES=FALSE \
+          -DACADOS_WITH_HPMPC=FALSE \
+          -DACADOS_WITH_QPDUNES=FALSE \
+          -DACADOS_WITH_OSQP=TRUE \
+          -DACADOS_PYTHON=TRUE \
+          -DACADOS_EXAMPLES=TRUE \
+          -DACADOS_INSTALL_DIR=${HOME}/acados_osqp6
+make -j16
+cp -r /tmp/acados/build ${HOME}/acados_osqp6
+make install
